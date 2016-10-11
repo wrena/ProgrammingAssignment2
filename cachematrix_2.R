3c7a9df72cbe0ef34b7ee69256a757e9f4ee2dac
wrena <ran.uwo@gmail.com>
2016-10-11 15:35:42 -0400

    Assignment2
    Solutions

--- cachematrix_2.R
+++ cachematrix_2.R
@@ -21,13 +21,5 @@ makeCacheMatrix <- function(x = matrix()) {
 ##then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
+        ## Return a matrix that is the inverse of 'x'
 }
